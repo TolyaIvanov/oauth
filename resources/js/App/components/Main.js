ba37f136385d2758401c2cf4612495503d036af8
TolyaIvanov <ivanov.anatoliu@list.ru>
2019-06-08 17:54:22 +0300

    first

---
+++ resources/js/App/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Provider} from 'react-redux'
+
+class Main extends React.Component {
+	render() {
+		return (
+				<div className="container">
+					main
+				</div>
+		);
+	}
+}
+
+export default Main;
